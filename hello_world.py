608197c5a4e81693119bb71d4b488a9484901451
Samuel Valdes <sv1220@ic.ac.uk>
2021-08-11 17:01:19 +0100

    first commit

---
+++ hello_world.py
@@ -0,0 +1,60 @@
+# Excercise 1
+
+print("Hello World!")
+
+# Excercise 2
+
+name = "Samuel"
+
+print("Hello",name,"!")
+
+# Excercise 3
+
+name = "Samuel"
+
+print("Hello " + name +"!")
+
+# Excercise 4
+
+number = 13
+
+print("Hello",number,"!")
+
+# Excercise 5 (BONUS)
+
+number = 13
+
+print("Hello "+str(number)+"!")
+
+# Excercise 6
+
+food_1 = "pizza"
+food_2 = "hamburgers"
+
+print(f"I love to eat {food_1} and {food_2}!")
+
+# Excercise 7
+
+food_1 = "pizza"
+food_2 = "hamburgers"
+
+print("I love to eat {} and {}!".format(food_1, food_2))
+
+
+# Excercise 8 (BONUS)
+
+sentence = "Hello my name is Samuel and I live in Santiago, Chile. It is the best place in Latin America"
+print(sentence)
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.count("is"))
+print(sentence.split())
+print(sentence.find("is"))
+print(sentence.isalnum())
+print(sentence.isalpha())
+
+a=("hello","how","are","you")
+print("#".join(a))
+
+print(sentence.endswith("america"))
+print(sentence.endswith("America"))
